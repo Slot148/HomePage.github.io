faf309ae60519a041a7859b8a90c4b7852cb8404
Nicolas <nicolasfreitas2004sp@gmail.com>
2024-08-25 17:31:48 -0300

    #

--- W95/W95.js
+++ W95/W95.js
@@ -1,7 +1,26 @@
-function createElement(tag, parent, id, content, classes) {
+fetch('W95.json')
+  .then(response => response.json())
+  .then(data =>{
+    console.log(data);
+  })
+  .catch(error => console.error('ERRO .JSON', error))
+
+  
+function createElement(
+  tag,
+  parent,
+  id,
+  content,
+  classes,
+  eventType,
+  eventFunction
+) {
   const element = document.createElement(tag);
   element.id = id;
   element.classList.add(...classes);
+  if (eventType && eventFunction) {
+    element.addEventListener(eventType, eventFunction);
+  }
   parent.appendChild(element);
   element.textContent = content;
   return element;
@@ -19,33 +38,21 @@ const taskbar = createElement("div", document.body, "taskbar", "", [
 ]);
 
 //botão iniciar
-const startbtn = createElement("button", taskbar, "btniniciar", "Iniciar", [
-  "borda1",
-]);
+const startbtn = createElement(
+  "button",
+  taskbar,
+  "btniniciar",
+  "Iniciar",
+  ["borda1"],
+  "click",
+  toggleBorder
+);
 
+//menuIniciar
 let menuIniciar;
 let decMenuIniciar;
 let windowsTextMenuBar;
 let w95TextMenuBar;
-
-startbtn.addEventListener("click", function (){
-  startbtn.classList.toggle("borda2");
-
-  if (!menuIniciar) {
-    menuIniciar = createElement("div", document.body, "menuiniciar", "", [
-      "menuiniciar",
-      "borda1",
-    ]);
-    decMenuIniciar = createElement("div", menuIniciar, "", "", ["barra"]);
-    windowsTextMenuBar = createElement("p", decMenuIniciar, "", "Mindows", [
-      "windows95txt",
-    ]);
-    w95TextMenuBar = createElement("p", decMenuIniciar, "", "95", ["w95"]);
-  } else {
-    menuIniciar?.remove();
-    menuIniciar = null;
-  }
-});
 
 //botão relogio
 const clock = createElement("button", taskbar, "menuclock", "00:00", [
@@ -67,3 +74,25 @@ function updateClock() {
 }
 
 setInterval(updateClock, 1000);
+
+function toggleBorder() {
+  this.classList.toggle("borda2");
+  toggleMenu();
+}
+
+function toggleMenu() {
+  if (!menuIniciar) {
+    menuIniciar = createElement("div", document.body, "menuiniciar", "", [
+      "menuiniciar",
+      "borda1",
+    ]);
+    decMenuIniciar = createElement("div", menuIniciar, "", "", ["barra"]);
+    windowsTextMenuBar = createElement("p", decMenuIniciar, "", "Mindows", [
+      "windows95txt",
+    ]);
+    w95TextMenuBar = createElement("p", decMenuIniciar, "", "95", ["w95"]);
+  } else {
+    menuIniciar?.remove();
+    menuIniciar = null;
+  }
+}
