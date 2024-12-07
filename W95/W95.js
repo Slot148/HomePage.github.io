899a6bed0c55ecafd484d3cbff081a3cd2a88bc9
Nicolas Anderson <nicolasfreitas2004sp@gmail.com>
2024-12-06 22:32:02 -0300

    #

--- W95/W95.js
+++ W95/W95.js
@@ -1,39 +1,40 @@
+//objetos
 const workSpace = {
   tag: "div",
   id: "workspace",
   classes: ["workspace"],
 };
 
-const taskBar = {
-  tag: "div",
-  id: "taskbar",
-  classes: ["taskbar"],
-};
-
-const startBtn = [{
-  tag: "button",
-  parent: "taskbar",
-  id: "btniniciar",
-  classes: ["buttonType1", "borda1"],
-  eventType: "click",
-  eventFunction: toggleBorder,
-},
-{
-  tag: 'span',
-  parent: 'btniniciar',
-  id: 'btnIniciarTextContent',
-  content: '<u>In</u>iciar',
-  classes: ['underline']
-},
-]
-
-const taskBarClock = {
-  tag: "button",
-  parent: "taskbar",
-  id: "taskBarClock",
-  content: "00:00",
-  classes: ["buttonType1", "borda2"],
-};
+const taskBar = [
+  {
+    tag: "div",
+    id: "taskbar",
+    classes: ["taskbar"],
+  },
+  {
+    tag: "button",
+    parent: "taskbar",
+    id: "btniniciar",
+    classes: ["buttonType1", "borda1"],
+    eventType: "click",
+    eventFunction: function () {
+      toggleState.call(this, menuIniciar);
+    },
+  },
+  {
+    tag: "span",
+    parent: "btniniciar",
+    id: "btnIniciarTextContent",
+    content: "<u>In</u>iciar",
+  },
+  {
+    tag: "button",
+    parent: "taskbar",
+    id: "taskBarClock",
+    content: "00:00",
+    classes: ["buttonType1", "borda2"],
+  },
+];
 
 const menuIniciar = [
   {
@@ -51,72 +52,166 @@ const menuIniciar = [
     tag: "p",
     parent: "decMenuIniciarBar",
     content: "Mindows",
-    classes: ["windows95txt", 'underline'],
+    classes: ["windows95txt"],
   },
   { tag: "p", parent: "decMenuIniciarBar", content: "95", classes: ["w95"] },
+  {
+    tag: "div",
+    parent: "menuIniciar",
+    id: "buttonBar",
+    classes: ["buttonBar"]
+  },
+  //buttons
+  {
+    tag: "button",
+    parent: "buttonBar",
+    id: "btn",
+    content: "Programs",
+    eventType: "click",
+    eventFunction: function () {
+      toggleState.call(this);
+    },
+  },
+  {
+    tag: "button",
+    parent: "buttonBar",
+    id: "btn2",
+    content: "Documents",
+    eventType: "click",
+    eventFunction: function () {
+      toggleState.call(this);
+    },
+  },
+  {
+    tag: "button",
+    parent: "buttonBar",
+    id: "btn3",
+    content: "Documents",
+    eventType: "click",
+    eventFunction: function () {
+      toggleState.call(this);
+    },
+  },
+  {
+    tag: "button",
+    parent: "buttonBar",
+    id: "btn4",
+    content: "Documents",
+    eventType: "click",
+    eventFunction: function () {
+      toggleState.call(this);
+    },
+  },
+  {
+    tag: "button",
+    parent: "buttonBar",
+    id: "help",
+    content: "Documents",
+    eventType: "click",
+    eventFunction: function () {
+      toggleState.call(this);
+    },
+  },
+  {
+    tag: "button",
+    parent: "buttonBar",
+    id: "run",
+    content: "Documents",
+    eventType: "click",
+    eventFunction: function () {
+      toggleState.call(this);
+    },
+  },
+  {
+    tag: "button",
+    parent: "buttonBar",
+    id: "shutdown",
+    content: "Documents",
+    eventType: "click",
+    eventFunction: function () {
+      toggleState.call(this);
+    },
+  },
 ];
 
-
-
-function construct(elements){
-  if(Array.isArray(elements)){
-    elements.forEach(item => constructStep2(item));
-  }else{
-    constructStep2(elements)
+//functions
+function construct(elements) {
+  if (Array.isArray(elements)) {
+    elements.forEach((item) => constructStep2(item));
+  } else {
+    constructStep2(elements);
   }
 }
 function constructStep2({
-tag,
-type,
-parent,
-id,
-content,
-classes,
-eventType,
-eventFunction,
+  tag,
+  type,
+  parent,
+  id,
+  content,
+  classes,
+  eventType,
+  eventFunction,
 }) {
-const element = document.createElement(tag);
-if ((tag === "input" || tag === "button") && type) element.type = type;
-if (id) element.id = id;
-if (classes) element.classList.add(...classes);
-if (eventType && eventFunction) {
-  element.addEventListener(eventType, eventFunction);
-}
-if (parent) {
-  const parentElement = document.getElementById(parent);
-  if (parentElement) {
-    parentElement.appendChild(element);
-  } else {
-    console.warn(`Elemento com id "${parent}" não encontrado. `);
+  const element = document.createElement(tag);
+  if ((tag === "input" || tag === "button") && type) element.type = type;
+  if (id) element.id = id;
+  if (classes) element.classList.add(...classes);
+  if (eventType && eventFunction) {
+    element.addEventListener(eventType, eventFunction);
   }
-} else {
-  document.body.appendChild(element);
-}
-if (content) {
-  if (tag === "input" || tag === "img") {
-    element.value = content;
-  } else if (tag === "a") {
-    element.href = content;
+  if (parent) {
+    const parentElement = document.getElementById(parent);
+    if (parentElement) {
+      parentElement.appendChild(element);
+    } else {
+      console.warn(`Elemento com id "${parent}" não encontrado. `);
+    }
   } else {
-    element.innerHTML = content;
+    document.body.appendChild(element);
+  }
+  if (content) {
+    if (tag === "input" || tag === "img") {
+      element.value = content;
+    } else if (tag === "a") {
+      element.href = content;
+    } else {
+      element.innerHTML = content;
+    }
+  }
+
+  return element;
+}
+
+function toggleState(objeto) {
+  let state = this.dataset.state === "true";
+  state = !state;
+  this.dataset.state = state;
+  if (state) {
+    this.classList.add("ativo");
+    console.log("yes");
+  } else {
+    this.classList.remove("ativo");
+    console.log("no");
+  }
+
+  if (objeto) {
+    if (Array.isArray(objeto)) {
+      const menuCondition = document.getElementById(objeto[0].id);
+      if (menuCondition) {
+        menuCondition.remove();
+      } else {
+        construct(objeto);
+      }
+    } else if (objeto && objeto.id) {
+      const menuCondition = document.getElementById(objeto.id);
+      if (menuCondition) {
+        menuCondition.remove();
+      } else {
+        construct(objeto);
+      }
+    }
   }
 }
-
-return element;
-}
-
-
-//workspace
-construct(workSpace);
-
-//taskbar
-construct(taskBar);
-
-//botão iniciar
-construct(startBtn);
-
-//botão relogio
-construct(taskBarClock);
 
 function updateClock() {
   const clockElement = document.getElementById("taskBarClock");
@@ -129,31 +224,11 @@ function updateClock() {
   const year = String(now.getFullYear()).slice(-2);
 
   clockElement.textContent = `${hours}:${minutes}`;
-  dateElement.textContent = `${day}/${month}/${year}`;
+  // dateElement.textContent = `${day}/${month}/${year}`;
 }
 setInterval(updateClock, 1000);
 
-function toggleBorder() {
-  this.classList.toggle("ativo");
-  toggleMenu(menuIniciar);
-}
-
-function toggleMenu(objeto) {
-  if (Array.isArray(objeto)) {
-    const menuCondition = document.getElementById(objeto[0].id);
-    if (menuCondition) {
-      menuCondition.remove();
-    } else {
-      construct(objeto)
-    }
-  } else if(objeto && objeto.id){
-    const menuCondition = document.getElementById(objeto.id);
-    if (menuCondition) {
-      menuCondition.remove();
-    } else {
-      construct(objeto);
-    }
-  }
-}
-
-
+//render
+construct(workSpace);
+construct(taskBar);
+construct(taskBarClock);
